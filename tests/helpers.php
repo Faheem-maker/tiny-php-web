29deb7637099c84821119daa3afb9a3ba4e68626
Faheem Anis <faheem@cartzlink.com>
2026-04-02 19:28:05 +0500

    Added application instantiation test

---
+++ tests/helpers.php
@@ -0,0 +1,72 @@
+<?php
+
+use framework\components\Config;
+use framework\components\PathManager;
+use framework\components\Validator;
+use framework\web\tests\TestDependencyContainer;
+use framework\web\WebApplication;
+
+function createApp(array $config = [])
+{
+    $base_config = [
+        'paths' => [
+            'base_dir' => __DIR__,
+            'root' => __DIR__,
+            'runtime' => __DIR__ . '/runtime',
+            'assets' => __DIR__ . '/app/resources',
+        ],
+        'TEST_KEY' => 'tests',
+    ];
+
+    $base_config = array_merge_recursive($base_config, $config);
+
+    $app = WebApplication::getInstance('/', 'GET');
+
+    $app->registerComponent('config', new Config());
+    $app->registerComponent('path', new PathManager());
+    $app->registerComponent('di', new TestDependencyContainer());
+    $app->registerComponent('validator', new Validator());
+
+    foreach ($base_config as $key => $value) {
+        $app->config->set($key, $value);
+    }
+
+    $app->init();
+
+    return $app;
+}
+
+function app()
+{
+    return WebApplication::get();
+}
+
+/**
+ * @return framework\db\QueryBuilder|null
+ */
+function db()
+{
+    return app()->db;
+}
+
+function config($key = null, $default = null)
+{
+    if ($key === null) {
+        return app()->config;
+    }
+
+    return app()->config->get($key, $default);
+}
+
+function env($key, $default = null)
+{
+    if ($key === null) {
+        return $_ENV;
+    }
+    return $_ENV[$key] ?? $default;
+}
+
+function logs()
+{
+    return app()->logger;
+}
